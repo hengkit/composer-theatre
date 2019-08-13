2dd0ed98c35ac63c139e05231f02f95fd965dc0b
Kit <kit@getpantheon.com>
2019-08-13 13:27:24 -0700

    8.7.6 core update

---
+++ web/private/scripts/slack_after_code_sync.php
@@ -0,0 +1,16 @@
+<?php
+// Load Slack helper functions
+require_once( dirname( __FILE__ ) . '/slack_helper.php' );
+if ( isset( $_POST['wf_type'] ) && $_POST['wf_type'] == 'sync_code' ) {
+  // Retrieve the committer, hash, and message for the most recent commit.
+  $committer = `git log -1 --pretty=%cn`;
+  $commit_message   = `git log -1 --pretty=%B`;
+  $hash      = `git log -1 --pretty=%h`;
+  // Setup the Text
+  $message = array();
+  $message['Commit Message'] = '"' . rtrim($commit_message) . '"' . "\n";
+  $message['Commit Details'] = 'Commit _' . rtrim($hash) . '_ to the `' . $_ENV['PANTHEON_ENVIRONMENT'] . '` environment.' . "\n";
+  $message['Committed By'] = rtrim($committer) . "\n";
+  $message['Site Name'] = '`' . $_ENV['PANTHEON_SITE_NAME'] . '`' . "\n";
+  _slack_tell( $message, 'sales-demo', 'Pantheon Git', 'http://dev-mikes-d8-demo.pantheonsite.io/sites/default/files/icons/git.png', '#666666');
+}
